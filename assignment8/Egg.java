146e20b98c2cb4f08dd4f789979a26ef0aad5b7b
Lin Shi <shilin.athena@gmail.com>
2012-04-15 18:15:51 -0400

    Add mushroom!!!
    So cute!!!

--- assignment8/Egg.java
+++ assignment8/Egg.java
@@ -7,11 +7,11 @@ public class Egg extends MISApplet {
 	double sqrt = 0;
 	double temp = 0;
 	double epsilon = 0.01;
-	double speed = 0.5;
+	double speed = 0.4;
 //	double background[] = {255.0, 255.0, 255.0};
 	double background[] = {0, 0, 0};
 //	double background[] = {65.0, 105.0, 225.0};
-	int geoI = 4;
+	int geoI = 5;
 	int traceTime = 0;
 	///////////// object parameters /////////////
 	// parameter of foundation
@@ -24,16 +24,22 @@ public class Egg extends MISApplet {
 	double pipeOut = 1;
 	double pipeIn = pipeOut*2.0/3.0;
 	double pipeH = pipeOut;
-	double pipeX = -2;
-	double pipeZ = -1;
+	double pipeX = -1;
+	double pipeZ = -0.5;
 	double pipeMC = 0.3;
 	double pipeP = 10;
+	// parameter of mushroom
+	double mushroomW = 0.5;
+	double mushroomH = mushroomW * 2.2;
+	double mushroomMC = 0.01;
+	double mushroomP = 10;
 
 	Geometry geoOrigin[] = new Geometry[geoI];
 	Geometry geometrys[] = new Geometry[geoI];
 	Material materials[] = new Material[geoI];
 	Light lights[] = new Light[1];
 	Matrix m = new Matrix();
+	HitList hit = new HitList();
 
 	double v[] = {0,0,0};	// original of the ray
 	double w[] = {0,0,0};	// direction of the ray
@@ -106,24 +112,89 @@ public class Egg extends MISApplet {
 		materials[2].setP(pipeP);
 		materials[2].setMCrgb(pipeMC);
 
+		// mushroom ^_^
 		geoOrigin[3] = new Geometry();
 		geoOrigin[3].Sphere();
 		m.identity();
-		m.translate(0.5,-1,pipeZ);
+		m.translate(0,foundPosition+mushroomW,0);
+		m.scale(mushroomW,mushroomW,mushroomW);
+		m.transform(geoOrigin[3]);
+//		geoOrigin[4].Plane(0,-1,0.3,foundPosition+mushroomW*0.4);
+		geoOrigin[3].Sphere();
+		m.identity();
+		m.translate(0,foundPosition+mushroomW,0);
+		m.translate(0,mushroomW*1.4,-mushroomW*0.6);
+		m.scale(mushroomW*2,mushroomW*2,mushroomW*2);
 		m.transform(geoOrigin[3]);
 
 		materials[3] = new Material();
-		materials[3].setArgb(0,0,1);	// green
+		materials[3].setArgb(1,0,0);	// red
 		materials[3].setDrgb(1.0,1.0,1.0);
 		materials[3].setSrgb(1.0,1.0,1.0);
-		materials[3].setP(pipeP);
-		materials[3].setMCrgb(pipeMC);
+		materials[3].setP(mushroomP);
+		materials[3].setMCrgb(mushroomMC);
 
+		geoOrigin[4] = new Geometry();
+		geoOrigin[4].Cylinder();
+		m.identity();
+		m.scale(mushroomW*0.6,1,mushroomW*0.6);
+		m.transform(geoOrigin[4]);
+//		geoOrigin[4].Plane(0,1,-0.3,-foundPosition-mushroomW*0.4);
+//		geoOrigin[4].Plane(0,-1,0,foundPosition);
+		geoOrigin[4].Sphere();
+		m.identity();
+		m.translate(0,foundPosition+mushroomW,0);
+		m.scale(mushroomW,mushroomW,mushroomW);
+		m.transform(geoOrigin[4]);
+		geoOrigin[4].Sphere();
+		m.identity();
+		m.translate(0,foundPosition+mushroomW,0);
+		m.translate(0,mushroomW*1.4,-mushroomW*0.6);
+		m.scale(mushroomW*2,mushroomW*2,mushroomW*2);
+		m.transform(geoOrigin[4]);
+		geoOrigin[4].reverse();
+
+		materials[4] = new Material();
+		materials[4].setArgb(255.0/255.0,204.0/255.0,102.0/255.0);	// skin
+		materials[4].setDrgb(1.0,1.0,1.0);
+		materials[4].setSrgb(1.0,1.0,1.0);
+		materials[4].setP(mushroomP);
+		materials[4].setMCrgb(mushroomMC); 
+
+		// mushroom ^_^
+/*		geoOrigin[4] = new Geometry();
+		geoOrigin[4].Sphere();
+/*		geoOrigin[4].Cylinder();
+		m.identity();
+//		m.translate(0.5,-1,pipeZ);	--------
+		m.scale(mushroomW*0.6,1,mushroomW*0.6);
+		m.transform(geoOrigin[4]);
+//		geoOrigin[4].Plane(0,-1,0.3,-0.1);
+		geoOrigin[4].Sphere();
+		m.identity();
+		m.translate(0,mushroomW*1.5,mushroomW*0.6);
+		m.scale(mushroomW*2,mushroomW*2,mushroomW*2);
+		m.transform(geoOrigin[4]);
+		geoOrigin[4].reverse(); */
+/*		geoOrigin[4].Sphere();
+		m.identity();
+//		m.translate(0,-mushroomW,0);
+		m.scale(mushroomW*2,mushroomW*2,mushroomW*2);
+		m.transform(geoOrigin[4]); */
+
+/*		materials[4] = new Material();
+		materials[4].setArgb(0,0,1);	// red
+		materials[4].setDrgb(1.0,1.0,1.0);
+		materials[4].setSrgb(1.0,1.0,1.0);
+		materials[4].setP(mushroomP);
+		materials[4].setMCrgb(mushroomMC); */
 
 		/////////////////// light //////////////////
 		lights[0] = new Light();
-		lights[0].setPosition(10, 0, 0);
+	//	lights[0].setPosition(10, 20, -10);
+		lights[0].setPosition(10, 2, 0);
 		lights[0].setIrgb(0.7, 0.7, 0.7);
+	//	lights[0].setIrgb(0.7, 0, 0);
 		normalize(lights[0].Lxyz);
 
 /*		lights[1] = new Light();
@@ -133,12 +204,14 @@ public class Egg extends MISApplet {
 	}
 
 	public void initFrame(double time) { // add animation here!!!!!!
-//		m.identity();
-//		m.rotateX(time * speed);
+		m.identity();
+		m.rotateY(-time * speed);
+		m.translate(3,0,0);
 		for (int iG=0; iG<geoOrigin.length; iG++) {
 			geometrys[iG] = geoOrigin[iG].clone();
-//			m.transform(geoOrigin[iG], geometrys[iG]);
 		}
+			m.transform(geoOrigin[3], geometrys[3]);
+			m.transform(geoOrigin[4], geometrys[4]);
 	}
 
 
@@ -348,24 +421,41 @@ public class Egg extends MISApplet {
 					phong[1] = Argb[1];
 					phong[2] = Argb[2];
 					// for each light!
+					// check for shadow
+					double newV[] = {0,0,0};
+					double newVshadow[] = {0,0,0};
+					double newW[] = {0,0,0};
+					newV[0] = S[0] + epsilon*R[0];
+					newV[1] = S[1] + epsilon*R[1];
+					newV[2] = S[2] + epsilon*R[2];
+					newW[0] = R[0];
+					newW[1] = R[1];
+					newW[2] = R[2];
+					normalize(newW);
 					for (int iL=0; iL<lights.length; iL++) {
 						Lxyz = lights[iL].Lxyz;
 						Irgb = lights[iL].Irgb;
+						newVshadow[0] = S[0] - epsilon*Lxyz[0];
+						newVshadow[1] = S[1] - epsilon*Lxyz[1];
+						newVshadow[2] = S[2] - epsilon*Lxyz[2];
+					if (traceTime <= 1) {
+						boolean shadow = shadowReturn(newV, Lxyz);
+						if (!shadow) {	// if this pixal is not in shadow
+							double dotLN = Math.max(0,dot(Lxyz,N));
+							double dotLV = Math.pow(Math.max(0,dot(Lxyz,R)),p);
+							phong[0] += Irgb[0]*(Drgb[0]*dotLN+Srgb[0]*dotLV);
+							phong[1] += Irgb[1]*(Drgb[1]*dotLN+Srgb[1]*dotLV);
+							phong[2] += Irgb[2]*(Drgb[2]*dotLN+Srgb[2]*dotLV);
+						}
+					} else {
 						double dotLN = Math.max(0,dot(Lxyz,N));
 						double dotLV = Math.pow(Math.max(0,dot(Lxyz,R)),p);
 						phong[0] += Irgb[0]*(Drgb[0]*dotLN+Srgb[0]*dotLV);
 						phong[1] += Irgb[1]*(Drgb[1]*dotLN+Srgb[1]*dotLV);
 						phong[2] += Irgb[2]*(Drgb[2]*dotLN+Srgb[2]*dotLV);
+					}
+
 					} 
-					double newV[] = {0,0,0};
-					newV[0] = S[0] + epsilon*R[0];
-					newV[1] = S[1] + epsilon*R[1];
-					newV[2] = S[2] + epsilon*R[2];
-					double newW[] = {0,0,0};
-					newW[0] = R[0];
-					newW[1] = R[1];
-					newW[2] = R[2];
-					normalize(newW);
 					reflection = getRayColor(newV, newW);
 					colorReturn[0] = phong[0]*(1.0-mc[0]) + reflection[0]*mc[0];
 					colorReturn[1] = phong[1]*(1.0-mc[1]) + reflection[1]*mc[1];
@@ -376,6 +466,134 @@ public class Egg extends MISApplet {
 			}
 			return colorReturn;
 		}
+	}
+
+	private boolean shadowReturn(double v[], double w[]) {
+		double t1 = 0, t2 = 0, t = 0;
+		boolean interact = false;
+		double S[] = {0,0,0};	// surface of the interact
+		double N[] = {0,0,0};	// surface normal for a sphere
+		double A = 0, B = 0, C = 0;
+		double a = 0, b = 0, c = 0, d = 0, e = 0, f = 0, g = 0, h = 0, i = 0, j = 0;	
+
+		for (int iG=0; iG<geometrys.length; iG++) {
+			for (int iSubG=0; iSubG<geometrys[iG].getSubNumber(); iSubG++) {
+				a = geometrys[iG].a[iSubG];
+				b = geometrys[iG].b[iSubG];
+				c = geometrys[iG].c[iSubG];
+				d = geometrys[iG].d[iSubG];
+				e = geometrys[iG].e[iSubG];
+				f = geometrys[iG].f[iSubG];
+				g = geometrys[iG].g[iSubG];
+				h = geometrys[iG].h[iSubG];
+				i = geometrys[iG].i[iSubG];
+				j = geometrys[iG].j[iSubG];
+				if (a!=0 || b!=0 || c!=0 || d!=0 || e!=0 || f!=0 ) {
+					A = a*w[0]*w[0] + b*w[1]*w[1] + c*w[2]*w[2] + 
+						d*w[1]*w[2] + e*w[2]*w[0] + f*w[0]*w[1];
+					B = 2.0*a*w[0]*v[0] + 2.0*b*w[1]*v[1] + 2.0*c*w[2]*v[2] +
+						d*v[1]*w[2]+d*w[1]*v[2] + e*v[2]*w[0]+e*w[2]*v[0]+ f*v[0]*w[1]+f*w[0]*v[1] +
+						g*w[0] + h*w[1] + i*w[2];
+					C = a*v[0]*v[0] + b*v[1]*v[1] + c*v[2]*v[2] +
+						d*v[1]*v[2] + e*v[2]*v[0] + f*v[0]*v[1] +
+						g*v[0] + h*v[1] + i*v[2] + j;
+					sqrt = B*B-4.0*A*C;
+					if (sqrt > 0) {	// if interact with sphere iG
+						double tempT1 = (-B - Math.sqrt(sqrt))/(2.0*A);
+						double tempT2 = (-B + Math.sqrt(sqrt))/(2.0*A);
+						t1 = tempT1 < tempT2 ? tempT1 : tempT2;
+						t2 = tempT1 > tempT2 ? tempT1 : tempT2;
+						if (t1 > 0) {	// t2 > 0
+							S[0] = w[0]*t1+v[0]; S[1] = w[1]*t1+v[1]; S[2] = w[2]*t1+v[2];
+							N[0] = 2.0*a*S[0] + f*S[1] + e*S[2] + g;
+							N[1] = 2.0*b*S[1] + d*S[2] + f*S[0] + h;
+							N[2] = 2.0*c*S[2] + e*S[0] + d*S[1] + i;
+							int in1 = dot(w,N)<0 ? 1 : -1;
+							S[0] = w[0]*t2+v[0]; S[1] = w[1]*t2+v[1]; S[2] = w[2]*t2+v[2];
+							N[0] = 2.0*a*S[0] + f*S[1] + e*S[2] + g;
+							N[1] = 2.0*b*S[1] + d*S[2] + f*S[0] + h;
+							N[2] = 2.0*c*S[2] + e*S[0] + d*S[1] + i;
+							int in2 = dot(w,N)<0 ? 1 : -1;
+							double thisList[][] = { {t1, iSubG, in1}, {t2, iSubG, in2} };
+							if (iSubG == 0) {
+								hit.Put(thisList);
+							} else {
+								if (hit.isEmpty())
+									break;
+								else {
+									hit.Union(thisList);
+								}
+							}
+							if (hit.isEmpty())
+								break;
+						} else if (t2 > 0) {
+							S[0] = w[0]*t2+v[0]; S[1] = w[1]*t2+v[1]; S[2] = w[2]*t2+v[2];
+							N[0] = 2.0*a*S[0] + f*S[1] + e*S[2] + g;
+							N[1] = 2.0*b*S[1] + d*S[2] + f*S[0] + h;
+							N[2] = 2.0*c*S[2] + e*S[0] + d*S[1] + i;
+							int in2 = dot(w,N)<0 ? 1 : -1;
+							double thisList[][] = { {t2, iSubG, in2} };
+							if (iSubG == 0) {
+								hit.Put(thisList);
+							} else {
+								if (hit.isEmpty())
+									break;
+								else {
+									hit.Union(thisList);
+								}
+							}
+							if (hit.isEmpty())
+								break;
+						} else
+							hit.empty();
+					} else {		// if not interact than empty the hitList
+						if (A < 0) {	// if it's a "reversed" graph
+							double thisList[][] = { {-1, iSubG, 1} };
+							if (iSubG == 0) {
+								hit.Put(thisList);
+							} else {
+								if (hit.isEmpty())
+									break;
+								else {
+									hit.Union(thisList);
+								}
+							}
+						} else
+							hit.empty();
+					}
+				} else {	// if it's plane, it must have a hitList (if t<0, than start from t=-1
+					double thisList[][] = { {-1,iSubG, 0} };
+					if (g*w[0] + h*w[1] + i*w[2] == 0) {	// if ray is parallel with the plane
+						thisList[0][2] = (g*v[0] + h*v[1] + i*v[2] + j)<0 ? 1 : -1;
+					} else {
+						t = -(g*v[0] + h*v[1] + i*v[2] + j)/(g*w[0] + h*w[1] + i*w[2]);
+						S[0] = w[0]*t+v[0]; S[1] = w[1]*t+v[1]; S[2] = w[2]*t+v[2];
+						N[0] = g;
+						N[1] = h;
+						N[2] = i;
+						thisList[0][2] = dot(w,N)<0 ? 1 : -1;
+						if (t>0) 	// if interact with the plane
+							thisList[0][0] = t;
+					}
+					if (iSubG == 0) {
+						hit.Put(thisList);
+					} else {
+						if (hit.isEmpty())
+							break;
+						else {
+							hit.Union(thisList);
+						}
+					}
+					if (hit.isEmpty())
+						break;
+				}
+			}
+			// after search each part in this geometry
+			if (!hit.isEmpty()) {
+				interact = true;
+			} 
+		}
+		return interact;
 	}
 
 	public double dot(int src[], double srr[]) {
